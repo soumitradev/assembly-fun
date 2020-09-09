572d6e823fff79a96d1a0701b4777c9956803597
Soumitra Shewale <soumitra.shewale.dev@gmail.com>
2020-09-09 19:46:19 +0530

    Add memcpy and scrolling while printing

---
+++ latest/kernel/utils.c
@@ -0,0 +1,9 @@
+// Write our own epic (unoptimised 😳) memcpy
+void memory_copy(char *source, char *dest, int nbytes)
+{
+    int i;
+    for (i = 0; i < nbytes; i++)
+    {
+        *(dest + i) = *(source + i);
+    }
+}
